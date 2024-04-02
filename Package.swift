f2b48ef2d4a7f4586f399eafa5c40eaf77401f80
CaKellum <christopherk379@gmail.com>
2024-04-02 19:41:46 -0400

    Update Documentation and privacy export

--- Package.swift
+++ Package.swift
@@ -19,7 +19,9 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SwiftRouting"),
+            name: "SwiftRouting",
+            path: "Sources",
+            resources: [.process("Resources/PrivacyInfo.xcprivacy")]),
         .testTarget(
             name: "SwiftRoutingTests",
             dependencies: ["SwiftRouting"])
---
+++ Sources/SwiftRouting.docc/SwiftRouting.md
@@ -0,0 +1,7 @@
+# ``SwiftRouting``
+
+This Is a simple framework to assist in implemting a Coordinator pattern for your application.
+
+## Overview
+
+Swift Routing tackles the issue of navigation code littering the Views. It also can be used for common functionallity like redirecting to other apps, or simply starting asynchronous tasks.
