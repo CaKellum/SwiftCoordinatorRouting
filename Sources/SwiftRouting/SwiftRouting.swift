0dbd98c1a30cd08ce0f036c6f24327befe671f7a
CaKellum <christopherk379@gmail.com>
2024-04-07 14:06:20 -0400

    Removing uneeded doc file, updating api access to the router and adding implemenrtation to the router Doc

--- Sources/SwiftRouting/SwiftRouting.swift
+++ Sources/SwiftRouting/SwiftRouting.swift
@@ -26,7 +26,7 @@ public protocol RouterDelegate: AnyObject {
 public class Router {
 
     /// the global Router Instance to limit passing of a router across the app
-    private(set) public static var instance: Router?
+    private static let instance: Router = Router()
 
     private var registeredRoutes = [String: Route]()
     private var registeredPreconditions = [String: Precondition]()
@@ -37,8 +37,10 @@ public class Router {
     /// initializes Router and its shared instance
     ///
     ///  Only ever initialize this once as early in the apps lifecycle for best performance
-    public init() {
-        Self.instance = self
+    private init() {}
+
+    public class func setDelegate(_ delegate: any RouterDelegate) {
+        Self.instance.delegate = delegate
     }
 
     /// when provided a route that exists this will execute that routes assignd operation
@@ -46,18 +48,18 @@ public class Router {
     /// - Parameter path: The desired route that needs to be routed to perform functions
     /// - Returns: If the route was successfully routed to then this retruns true, false otherwise
     @discardableResult
-    public func route(to path: String) -> Bool {
+    public class func route(to path: String) -> Bool {
         let basePath = path.removeQueryParams() ?? ""
-        guard let route = registeredRoutes[basePath] else {
-            delegate?.failedToRoute(to: path)
+        guard let route = instance.registeredRoutes[basePath] else {
+            instance.delegate?.failedToRoute(to: path)
             return false
         }
 
-        guard resolvePreconditions(for: route, with: path.getQueryParams()) else { return false }
+        guard instance.resolvePreconditions(for: route, with: path.getQueryParams()) else { return false }
 
-        delegate?.willRoute(to: path)
+        instance.delegate?.willRoute(to: path)
         route.operation(basePath, path.getQueryParams())
-        delegate?.didRoute(to: path)
+        instance.delegate?.didRoute(to: path)
         return true
     }
 
@@ -68,13 +70,14 @@ public class Router {
     /// - Returns: if the route passed the precondition it will return true, false otherwise
     private func resolvePreconditions(for route: Route, with params: [String: String]) -> Bool {
         let navigatedRoute = route.path.addQueryParams(params: params).orEmpty
+        // makes it so the we can resolve them all
         let routePreconditionActions = registeredPreconditions.filter { (key, _) in
             route.preconditions.contains(where: {$0 == key})
         }
         for (key, precondition) in routePreconditionActions {
             let newRoute = precondition.action(navigatedRoute)
             if navigatedRoute.removeQueryParams() != route.path {
-                self.route(to: newRoute)
+                Self.route(to: newRoute)
                 delegate?.failedPreconditon(key, for: navigatedRoute)
                 return false
             }
@@ -84,50 +87,50 @@ public class Router {
 
     /// registers the route in the router so it is aware it exists
     /// - Parameter registeringRoute: the route to be registered
-    public func register(_ registeringRoute: Route) {
-        registeredRoutes[registeringRoute.path] = registeringRoute
+    public class func register(_ registeringRoute: Route) {
+        instance.registeredRoutes[registeringRoute.path] = registeringRoute
     }
 
     /// registers a precondition so the route can be aware of it
     /// - Parameter precondition: THe Precondition to be registered
-    public func registerPrecondition(_ precondition: Precondition) {
-        registeredPreconditions[precondition.preconditionID] = precondition
+    public class func registerPrecondition(_ precondition: Precondition) {
+        instance.registeredPreconditions[precondition.preconditionID] = precondition
     }
 
     /// this will asks if the router has a route
     /// - Parameter path: the path you are looking for
     /// - Returns: true if router has rotue, false otherwise
-    public func hasRoute(path: String) -> Bool {
-        registeredRoutes[path.removeQueryParams() ?? ""] != nil
+    public class func hasRoute(path: String) -> Bool {
+        instance.registeredRoutes[path.removeQueryParams() ?? ""] != nil
     }
 
     /// remove a route form the router
     /// - Parameter route: the route's path to be removed
-    public func unregister(route: String) {
-        registeredRoutes.removeValue(forKey: route)
+    public class func unregister(route: String) {
+        instance.registeredRoutes.removeValue(forKey: route)
     }
 
-    /// remove a route form the router, this is a wrapper for ``unregister(route:)-5vkjw``
+    /// remove a route form the router, this is a wrapper for ``unregister(route:)-3n57n``
     /// - Parameter route: the route to be removed
-    public func unregister(route: Route) {
+    public class func unregister(route: Route) {
         unregister(route: route.path)
     }
 
     /// Adds many rotues ot the routere at once, convinece wrapper to the ``register(_:)`` funcition
     /// - Parameter routes: an array of routes to be added
-    public func massAdd(routes: [Route]) {
-        routes.forEach({self.register($0)})
+    public class func massAdd(routes: [Route]) {
+        routes.forEach({register($0)})
     }
 
-    /// Remove all routes that have the included paths, convinece wrapper to ``unregister(route:)-5vkjw``
+    /// Remove all routes that have the included paths, convinece wrapper to ``unregister(route:)-11wt2``
     /// - Parameter routes:an array of route paths to be remove from the router
-    public func massRemove(routes: [String]) {
-        routes.forEach({self.unregister(route: $0)})
+    public class func massRemove(routes: [String]) {
+        routes.forEach({unregister(route: $0)})
     }
 
-    /// Remove all routes that have the included in the array, convinece wrapper to ``unregister(route:)-1aq12``
+    /// Remove all routes that have the included in the array, convinece wrapper to ``unregister(route:)-3n57n``
     /// - Parameter routes:an array of routes to be removed, from the router
-    public func massRemove(routes: [Route]) {
-        routes.forEach({self.unregister(route: $0)})
+    public class func massRemove(routes: [Route]) {
+        routes.forEach({unregister(route: $0)})
     }
 }
