de3590ffcc508bd780c5aa153fe32174e8f79ebf
CaKellum <christopherk379@gmail.com>
2024-07-20 01:41:31 -0400

    Removing the testExample

--- Tests/SwiftRoutingTests/SwiftRoutingTests.swift
+++ Tests/SwiftRoutingTests/SwiftRoutingTests.swift
@@ -2,13 +2,6 @@ import XCTest
 @testable import SwiftRouting
 
 final class SwiftRoutingTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
-    }
 
     // MARK: - Testing string extensions
 
